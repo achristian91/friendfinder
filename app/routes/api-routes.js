db2cde265014fef11a2133f77cd25408b4767b6d
Arthur Christian <32134423+achristian91@users.noreply.github.com>
2018-11-19 10:33:37 -0500

    api routes logic

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,12 +1,52 @@
 var friends = require("../data/friends.js");
 
-module.exports = function(app) {
-    
+module.exports = function (app) {
+
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
     app.post("/api/friends", function (req, res) {
-        res.json(friends);
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifferences: 1000
+        };
+
+        console.log(req.body);
+
+        // taking the result of the users survey post         
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        console.log(userScores);
+
+        var totalDifference = 0;
+
+        // Nested forloop that loops all friends into database 
+        for (var i = 0; i < friends.length; i++) {
+
+            console.log(friends[i]);
+            totalDifference = 0;
+
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                if (totalDifference <= bestMatch.friendDifferences) {
+                    // reset match
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifferences = totalDifference;
+                }
+            }
+        }
+        // Saves User data to the data base 
+        friends.push(userData);
+
+        // Return a JSON with the user match 
+        res.json(bestMatch);
     });
+
 }
