e189ddac742fd8fe94b2167aa7c2486973cc67c0
Arthur Christian <32134423+achristian91@users.noreply.github.com>
2018-11-19 10:13:11 -0500

    added api routes

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 
 app.use(bodyParser.text({ type: 'text/html' }))
 
+require("./app/routes/api-routes.js")(app);
 require("./app/routes/html-routes.js")(app);
-
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
